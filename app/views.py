704b2915ef0ddcf5f42e190d98dcca2af0c38de3
mringeling17 <matias.ringeling@mail.udp.cl>
2021-06-10 15:04:41 -0400

    agregado lo pendiente, falta dejar funcionando

--- app/views.py
+++ app/views.py
@@ -135,3 +135,42 @@ def disponibilidad():
 @app.route('/add_reserva', methods = ['POST','GET'])
 def add_reserva():
 	return "Hay que implementarlo :("
+
+
+
+'''app.config['MAIL_SERVER'] = 'sirca.cuy.cl'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'sirca@cuy.cl'
+app.config['EMAIL_PASSWORD'] = 'OXurpKj708'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+@app.route("/confirmation")
+def confirmation():
+    msg = Message('Hola', sender='sirca@cuy.cl', recipients='mringeling1@gmail.com')
+    msg.body = "Reserva creada exitosamente"
+    mail.send(msg)
+    return "Enviado"
+
+
+@app.route('/reset/<token>', methods = ["GET", "POST"])
+def reset_with_token(token):
+    try:
+        email = ts.loads(token, salt="recover-key", max_age = 86400)
+    except:
+        abort(404)
+
+    form = PasswordForm()
+    
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=email).first_or_404()
+
+        user.password = form.password.data
+
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect(url_for('signin'))
+    return render_template('reset_with_token.html', form = form, token = token)'''
