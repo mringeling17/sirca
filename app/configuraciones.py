148ff091fef36b227a2d727a184703ea6dfbe431
Debian <henry@sirca.mpfp43kmn0gelc4sw54titjeig.bx.internal.cloudapp.net>
2021-06-10 03:42:20 +0000

    ordenando

---
+++ app/configuraciones.py
@@ -0,0 +1,7 @@
+db_host = "localhost"
+db_port = 5432
+db_user = "sirca"
+db_passwd = "?0X^F3m6&mvs"
+db_database = "sirca"
+salt = b'$2b$12$HlbE43.Fp9QvJiPjtId9tO'
+session_key = 'DQ7aqJyTAogoBDRumKuMuvV1kA2no9aX'
---
+++ app/setup.py
@@ -0,0 +1,36 @@
+from configuraciones import *
+import psycopg2
+conn = psycopg2.connect("dbname=%s user=%s password=%s host=%s port=%s"%(db_database,db_user,db_passwd,db_host,db_port))
+
+
+cur = conn.cursor()
+sql ="""DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;"""
+
+cur.execute(sql)
+
+sql ="""
+CREATE TABLE usuarios
+           (id serial PRIMARY KEY, email varchar(100) unique, password varchar(255), nombre varchar(80), apellido varchar(80), tipo integer, nivel integer, fecha_registro timestamp);
+"""
+
+cur.execute(sql)
+
+sql = """
+CREATE TABLE reservas
+            (id serial PRIMARY KEY, disponible bool, dia integer, mes integer, anno integer, bloque integer, jugador1 integer, jugador2 integer, invitado1 varchar(160), invitado2 varchar(160), cancha integer, tx1 integer, tx2 integer, tipo_reserva integer, pago integer, fecha_reserva timestamp);
+"""
+
+cur.execute(sql)
+
+sql = """
+CREATE TABLE transacciones
+            (id serial PRIMARY KEY, id_usuario integer, url varchar(255), fecha timestamp)
+"""
+
+cur.execute(sql)
+
+
+conn.commit()
+cur.close()
+conn.close()
