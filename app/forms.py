610d3e6ae4662fd5f58a71cb88c041a42924f05f
mringeling17 <matias.ringeling@mail.udp.cl>
2021-06-11 01:05:32 -0400

    disponibilidad no registrados

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, validators
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email
 
-class EmailForm(Form):
-    email = StringField('Email', validators= [DataRequired(), Email()])
+class ForgotForm(Form):
+    email = EmailField('Email Address', [validators.data_required(), validators.email()])
 
-class PasswordForm(Form):
-    email = PasswordField('Email', validators=[DataRequired(), Email()])
+class PasswordResetForm(Form):
+    current_password = PasswordField('Current Password', [validators.DataRequired(), validators.length(min=6, max=80)])
