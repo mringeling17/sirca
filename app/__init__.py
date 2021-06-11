934cb7e5c27d454c998024a2142644d19d3a1e68
mringeling17 <matias.ringeling@mail.udp.cl>
2021-06-10 21:46:47 -0400

    1

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_mail import Mail
 
 app = Flask(__name__)
-from app import views
 app.config['MAIL_SERVER'] = 'sirca.cuy.cl'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'sirca@cuy.cl'
