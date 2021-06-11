f734f5be254537dd13fa4dd7acbfdfc35c0d1631
root <root@sirca.mpfp43kmn0gelc4sw54titjeig.bx.internal.cloudapp.net>
2021-06-11 00:46:41 -0400

    arreglado auth email

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 from app import views
-app.config['MAIL_SERVER'] = 'mail.cui.cl'
+app.config['MAIL_SERVER'] = 'mail.cuy.cl'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'sirca@mail.cuy.cl'
+app.config['MAIL_USERNAME'] = 'sirca@cuy.cl'
 app.config['MAIL_DEFAULT_SENDER'] = 'sirca@cuy.cl'
 app.config['MAIL_PASSWORD'] = 'OXurpKj708'
 app.config['MAIL_USE_TLS'] = True
