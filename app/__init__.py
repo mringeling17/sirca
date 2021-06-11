2c1f67e52708c54e74b173a37fd9c137e31617ca
mringeling17 <matias.ringeling@mail.udp.cl>
2021-06-11 00:43:36 -0400

    correo2

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 from app import views
 app.config['MAIL_SERVER'] = 'mail.cui.cl'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'sirca@mail.cuy.cl'
 app.config['MAIL_DEFAULT_SENDER'] = 'sirca@cuy.cl'
 app.config['MAIL_PASSWORD'] = 'OXurpKj708'
