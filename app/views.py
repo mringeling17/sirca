a150cdff90ab34e30b1ba7831c64032fbe81297d
mringeling17 <matias.ringeling@mail.udp.cl>
2021-06-30 01:25:33 -0400

    if

--- app/views.py
+++ app/views.py
@@ -358,28 +358,30 @@ def validate_token(id):
 
 @app.route("/reset2/<id>", methods=["GET","POST"])
 def reset2(id):
-	if validate_token(id):
-		sql = "select email from token where '%s' = token_id"%id
-		cur2.execute(sql)
-		correo = cur2.fetchone()
-		if request.form["password"] != request.form["password2"]:
-			print("las contraseñas deben coincidir")#-->hacer con un flash en todos los print
-		if len(request.form["password"])<8:
-			print("la contraseña debe tener al menos 8 caracteres")
-		pwd = request.form["password"]
-		user_reset = "update usuarios set password =crypt('%s', gen_salt('bf') where email = '%s'), used = TRUE "%(pwd,correo)
-		try:
-			cur.execute(user_reset)
-			conn.commit()
-		except:
-			print("hubo un error al realizar la solicitud")
-			return redirect(url_for('/'))
-		print("Contraseña actualizada con exito")
-		return 	render_template("/login")
+	if id:
+		if validate_token(id):
+			sql = "select email from token where '%s' = token_id"%id
+			cur2.execute(sql)
+			correo = cur2.fetchone()
+			if request.form["password"] != request.form["password2"]:
+				print("las contraseñas deben coincidir")#-->hacer con un flash en todos los print
+			if len(request.form["password"])<8:
+				print("la contraseña debe tener al menos 8 caracteres")
+			pwd = request.form["password"]
+			user_reset = "update usuarios set password =crypt('%s', gen_salt('bf') where email = '%s'), used = TRUE "%(pwd,correo)
+			try:
+				cur.execute(user_reset)
+				conn.commit()
+			except:
+				print("hubo un error al realizar la solicitud")
+				return redirect(url_for('/'))
+			print("Contraseña actualizada con exito")
+			return 	render_template("/login")
+		else:
+			print("token invalido")
+			render_template("/login")
 	else:
-		print("token invalido")
-		render_template("/login")
-	
+		return "<h1>NULL TOKEN</h1>"
 @app.route('/myuser', methods = ['POST','GET']) #ver/actualizar datos del usuario y gurdar en la base
 def myuser():
 	if request.method == 'POST':
