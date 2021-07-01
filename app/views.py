89c260b3714b3c74f2cd5c139b137afb24380c77
Vicente henriquez <vicente.henriquezp@mail.udp.cl>
2021-06-30 22:36:43 -0400

    pr1

--- app/views.py
+++ app/views.py
@@ -130,7 +130,7 @@ def eliminar_reserva():
 				cur2.execute(sql)
 				dato = cur2.fetchone()
 				idjugador = int(dato[0])
-				sql="""select * from reservas where jugador1 = '%s'"""%(idjugador)
+				sql="""select * from reservas where jugador1 = '%s' and jugador2 = None and invitado1 = None"""%(idjugador)
 				cur2.execute(sql)
 				datos = cur2.fetchall()
 				return render_template("tabla_eliminar.html",datos=datos)
